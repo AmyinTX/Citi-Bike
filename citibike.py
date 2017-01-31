4edd8c007990d15658a06198190bc2baa993bfaf
Amy Brown <amybrown1130@gmail.com>
2017-01-30 18:29:37 -0600

    updated

--- citibike.py
+++ citibike.py
@@ -109,6 +109,9 @@ import sqlite3 as lite
 con = lite.connect('citi_bike.db')
 cur = con.cursor()
 
+#with con:
+#    cur.execute('delete from available_bikes')
+
 with con:
     cur.execute('CREATE TABLE citibike_reference (id INT PRIMARY KEY, totalDocks INT, city TEXT, altitude INT, stAddress2 TEXT, longitude NUMERIC, postalCode TEXT, testStation TEXT, stAddress1 TEXT, stationName TEXT, landMark TEXT, latitude NUMERIC, location TEXT )')
     
@@ -167,3 +170,73 @@ with con:
 
 
 ##### The code only needs to run for an hour. If it's sleeping every minute, the code only needs to loop 60 times. Find a way of doing this. 
+
+import time
+from dateutil.parser import parse
+import collections
+import sqlite3 as lite
+import requests
+
+con = lite.connect('citi_bike.db')
+cur = con.cursor()
+
+for i in range(60):
+    r = requests.get('http://www.citibikenyc.com/stations/json')
+    exec_time = parse(r.json()['executionTime']).strftime("%s")
+
+    cur.execute('INSERT INTO available_bikes (execution_time) VALUES (?)', (exec_time,))
+
+    for station in r.json()['stationBeanList']:
+        cur.execute("UPDATE available_bikes SET _%d = %d WHERE execution_time = %s" % (station['id'], station['availableBikes'], exec_time))
+    con.commit()
+
+    time.sleep(60)
+
+con.close() #close the database connection when done
+
+import pandas as pd
+import sqlite3 as lite
+
+con=lite.connect('citi_bike.db')
+cur = con.cursor
+
+# start with this line
+df = pd.read_sql_query('select * from available_bikes order by execution_time', con, index_col='execution_time')
+
+#df.to_pickle('/Users/amybrown/Thinkful/Unit_3/Lesson_1/citybike_df.csv') doesn't write file cleanly
+
+hour_change = collections.defaultdict(int)
+for col in df.columns:
+    station_vals = df[col].tolist()
+    station_id = col[1:] #trim the "_"
+    station_change = 0
+    for k,v in enumerate(station_vals):
+        if k < len(station_vals) - 1:
+            station_change += abs(station_vals[k] - station_vals[k+1])
+    hour_change[int(station_id)] = station_change #convert the station id back to integer
+    
+def keywithmaxval(d):
+    """Find the key with the greatest value"""
+    return max(d, key=lambda k: d[k])
+
+# assign the max key to max_station
+max_station = keywithmaxval(hour_change)
+
+#query sqlite for reference information
+
+from datetime import date
+
+con = lite.connect('citi_bike.db')
+cur = con.cursor()
+cur.execute("SELECT id, stationname, latitude, longitude FROM citibike_reference WHERE id = ?", (max_station,))
+data = cur.fetchone()
+print("The most active station is station id %s at %s latitude: %s longitude: %s " % data)
+print("With %d bicycles coming and going in the hour between %s and %s" % (
+    hour_change[max_station],
+    date.fromtimestamp(int(df.index[0])).strftime('%Y-%m-%dT%H:%M:%S'),
+    date.fromtimestamp(int(df.index[-1])).strftime('%Y-%m-%dT%H:%M:%S'),
+))
+
+import matplotlib.pyplot as plt
+plt.bar(hour_change.keys(), hour_change.values())
+plt.show()
